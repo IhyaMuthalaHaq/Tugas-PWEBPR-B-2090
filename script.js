3400d6092627bb6851209e603cc233ca64544b6f
IhyaMuthalaHaq <117703419+IhyaMuthalaHaq@users.noreply.github.com>
2023-09-24 23:35:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const signinBtn = document.getElementById("signin-button");
+
+    // Menambahkan tindakan (action) saat tombol diklik
+    signinBtn.addEventListener("click", function () {
+        // Mengarahkan pengguna ke halaman baru
+        window.location.href = "halaman-baru.html";
+    });
+
+    
